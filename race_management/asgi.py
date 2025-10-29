b0112b61477bbfcef376f15c58009671a4a3bc30
Syed Sajid Shah <40624065+sajeee@users.noreply.github.com>
2025-10-29 01:49:30 -0700

    Update asgi.py

--- race_management/asgi.py
+++ race_management/asgi.py
@@ -6,5 +6,9 @@ import tracking.routing
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'race_management.settings')
 
 django_asgi_app = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": URLRouter(tracking.routing.websocket_urlpatterns),
+})
 
 
