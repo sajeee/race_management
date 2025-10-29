253b291c1d6e470fd2630749d76fd5f6acdaade5
Syed Sajid Shah <40624065+sajeee@users.noreply.github.com>
2025-10-29 01:41:20 -0700

    Update settings.py

--- race_management/settings.py
+++ race_management/settings.py
@@ -6,7 +6,7 @@ import dj_database_url  # <-- add this package to requirements
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET', 'dev-secret')
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
 ALLOWED_HOSTS = ["*"]
 # Add Railway domain automatically
 
