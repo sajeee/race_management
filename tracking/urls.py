e62654c7bf528ec6abd2a3fd805e3552fa6b3fe5
Syed Sajid Shah <40624065+sajeee@users.noreply.github.com>
2025-10-30 23:29:45 -0700

    Update urls.py

--- tracking/urls.py
+++ tracking/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("race/<int:race_id>/dashboard/", views.dashboard, name="dashboard"),
     path("api/tracking/<int:race_id>/post_location/", views.post_location, name="post_location"),
+    path("api/tracking/<int:race_id>/latest/", views.get_latest_locations, name="get_latest_locations"),
 ]
