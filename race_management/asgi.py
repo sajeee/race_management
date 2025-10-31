7f31cb9616c27564094c12fe55e09270e41348d4
Syed Sajid Shah <40624065+sajeee@users.noreply.github.com>
2025-10-31 11:44:02 -0700

    Update asgi.py

--- race_management/asgi.py
+++ race_management/asgi.py
@@ -1,12 +1,16 @@
+# race_management/asgi.py
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import tracking.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'race_management.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "race_management.settings")
 
+# First, initialize Django (this calls django.setup())
 django_asgi_app = get_asgi_application()
+
+# Now safe to import anything using models
+import tracking.routing
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
