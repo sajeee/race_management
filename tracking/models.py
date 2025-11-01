8ebc95ae41f522445d097aa81808742d1ccc3830
Syed Sajid Shah <40624065+sajeee@users.noreply.github.com>
2025-10-31 22:28:32 -0700

    Update models.py

--- tracking/models.py
+++ tracking/models.py
@@ -1,3 +1,4 @@
+#tracking/models.py
 from django.contrib.gis.db import models
 from registration.models import Runner
 from core.models import Race
@@ -5,7 +6,7 @@ from core.models import Race
 class TrackingPoint(models.Model):
     runner = models.ForeignKey(Runner, on_delete=models.CASCADE)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
-    location = models.PointField()
+    location = models.PointField()  # GIS Field!
     timestamp = models.DateTimeField(auto_now_add=True)
 
     class Meta:
