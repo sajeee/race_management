2c98b80d6d0f734a4ac8ad0d675bad705383f2ae
Syed Sajid Shah <40624065+sajeee@users.noreply.github.com>
2025-10-31 23:11:41 -0700

    Update tasks.py

--- tracking/tasks.py
+++ tracking/tasks.py
@@ -1,1 +1,9 @@
+# tracking/tasks.py
+from celery import shared_task
+from .management.commands.archive_old import Command as ArchiveCommand
+
+@shared_task
+def archive_old():
+    cmd = ArchiveCommand()
+    cmd.handle()
 
